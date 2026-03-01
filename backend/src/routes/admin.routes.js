445d64380ba5e08222d1af3623388f2c0de66a89
ritesh455 <riteshpilavare9011@gmail.com>
2026-03-01 18:07:59 +0530

    Some bugs are solved

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const { addEmployee } = require("../controllers/admin.controller");
+
 const { requireAuth, requireAdmin } = require("../middleware/auth.middleware");
 
 const router = express.Router();
