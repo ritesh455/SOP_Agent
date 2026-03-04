f1a1ec715388b1443534e0688f0833de1448067c
ritesh455 <riteshpilavare9011@gmail.com>
2026-03-04 19:41:08 +0530

    Developing frontend part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,53 @@
-
-import './index.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import AdminDashboard from "./pages/AdminDashboard";
 
 function App() {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
 
   return (
-    <>
-     <div className='h-20 text-2xl'>
-      Hello
-     </div>
-    </>
-  )
+    <Routes>
+      {/* Default route */}
+      <Route
+        path="/"
+        element={
+          token
+            ? role === "admin"
+              ? <Navigate to="/admin" />
+              : <Navigate to="/employee" />
+            : <Navigate to="/login" />
+        }
+      />
+
+      {/* Login */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Employee */}
+      <Route
+        path="/employee"
+        element={
+          token && role === "employee"
+            ? <EmployeeDashboard />
+            : <Navigate to="/login" />
+        }
+      />
+
+      {/* Admin */}
+      <Route
+        path="/admin"
+        element={
+          token && role === "admin"
+            ? <AdminDashboard />
+            : <Navigate to="/login" />
+        }
+      />
+
+      {/* Catch-all */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
